70cc6d8f1ddea8081b49b199e0e3fc863a1d652d
bborsari <beatrice.borsari01@estudiant.upf.edu>
2018-01-25 10:34:25 +0100

    Retrieve for a given entry counts for all the keys.

--- utils/make.column.dictionary.py
+++ utils/make.column.dictionary.py
@@ -26,14 +26,21 @@ open_input = sys.stdin if options.input == "stdin" else open(options.input)
 #********
 
 d={}
+vals=[]
+
 for line in open_input.readlines():
 	name, val = line.strip().split('\t')
 	d[name] = d.get(name, {})
 	d[name][val] = d[name].get(val, 0) +1
+	vals.append(val)
 
+vals = set(vals)
+ 
 for key in d.keys():
 	mylist = []
-	for name in d[key].keys():
-		mystring = ":".join([name, str(d[key][name])])
+	for val in vals:
+		if (val not in d[key].keys()):
+			d[key][val] = 0
+		mystring = ":".join([val, str(d[key][val])])
 		mylist.append(mystring)
 	print key, "\t", ";".join(mylist)
